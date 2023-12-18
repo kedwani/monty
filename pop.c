c58fb9d674e51a8ad014dcca8381618d8bd1a40f
kedwani linux <mahmoudnasser11566@gmail.com>
2023-12-18 12:16:42 +0200

    auto push

--- pop.c
+++ pop.c
@@ -1,11 +1,22 @@
 #include "monty.h"
 void pop(void)
 {
+	stack_t *tmp;
+	
 	if (v.h)
 	{
-		v.h = v.h->next;
-		free(v.h->prev);
-		v.h->prev = NULL;
+		if (v.h->next == NULL)
+		{
+			tmp = v.h;
+			free(tmp);
+			v.h = NULL;
+		}
+	else
+		{
+			v.h = v.h->next;
+			free(v.h->prev);
+			v.h->prev = NULL;
+		}
 	}
 	else
 		error(7);
